f3baa625acd03946e6bebc527781b142e9ba1b6a
Angel Marco Cobos <angelcobos380@gmail.com>
2026-09-15 18:41:24 -0600

    feat(app.js): Agregar funcionalidad al presupuesto restante

--- js/app.js
+++ js/app.js
@@ -22,6 +22,12 @@ class Presupuesto {
 
     nuevoGasto(gasto){
         this.gastos = [...this.gastos, gasto];
+        this.calcularRestante();
+    }
+
+    calcularRestante(){
+        const gastado = this.gastos.reduce( (total,gasto) => total + gasto.cantidad, 0);
+        this.restante = this.presupuesto - gastado;
     }
 }
 class UI {
@@ -69,7 +75,7 @@ class UI {
             nuevoGasto.dataset.id = id; 
 
             //Agregar el HTML del Gasto 
-            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> ${cantidad} </span> `;
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> $ ${cantidad} </span> `;
 
             //Boton para borrar el gasto
             const btnBorrar = document.createElement("button");
@@ -86,6 +92,9 @@ class UI {
         while(gastoListado.firstChild){
             gastoListado.removeChild(gastoListado.firstChild);
         }
+    }
+    actualizarRestante(restante){
+    document.querySelector("#restante").textContent = restante;
     }
 }
 
@@ -139,9 +148,10 @@ const cantidad = Number(document.querySelector("#cantidad").value);
     ui.imprimirAlerta("Gasto Agregado Correctamente");
 
     // Imprimir los gastos 
-    const {gastos} = presupuesto;
+    const {gastos, restante } = presupuesto;
     ui.agregarGastoListado(gastos)
 
+    ui.actualizarRestante(restante);
     //Reiniciar Formulario
     formulario.reset();
 }
